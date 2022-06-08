846a45e0f199afd791556148995a765f98d69355
Goodness Nwaneri <105147555+Lindamami@users.noreply.github.com>
2022-06-08 22:48:17 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,18 @@ void print_alphabet_x10(void)
 	char alphabet;
         char x;
 
-	for (alphabet = 'a'; alphabet  <= 'z'; alphabet++)
+
+        x = 1;
+        while (x <= 10)
+
 	{
-		for (x = 1; x <= 10; x++)
+		for (alphabet = 'a'; alphabet  <= 'z'; alphabet++)
                    { 
-                       _putchar("%c\n”, alphabet);
-                       _putchar(x);
+                       _putchar(alphabet);
                    }
+
+                 x++;
+                 _putchar(’\n’);
 	}
 	
         return 0;
