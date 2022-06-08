ed8da7dfd3b75f48d136921d087a6b6bfe7515d3
Goodness Nwaneri <105147555+Lindamami@users.noreply.github.com>
2022-06-08 23:01:32 +0100

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase characters 
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+int a;
+
+for (a = 97; a <= 122; a++)
+{
+
+if (c == ‘a’)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
