113087d07e462e4203c46942aff9e3572041753f
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-09 13:08:41 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - checks for uppercase characters
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c = 'A'
+	{
+		if (c <= 'Z')
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	
+	return (0);
+}
