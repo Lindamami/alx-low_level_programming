4fe8c1b6a6fa01d0d649bd2c926d7381d8d0e481
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-11 22:11:28 -0700

    prints alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the lowercase alphabet followed by a new line.
+ * main - Prints the lowercase alphabet in reverse
+ * followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; --alphabet)
-	{
-		putchar("%c", alphabet);
-	}
-	
+		putchar(alphabet);
+       	
 	putchar('\n');
 
 	return (0);
