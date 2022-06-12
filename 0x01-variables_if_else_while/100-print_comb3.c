f90c3ad723f58c4a0f6b88c5633b4c915b0a7f82
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-12 01:56:13 -0700

    Prints all possible variations of 2 and 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,16 @@ int main(void)
 
 	for (num = 2; num <= 3; num++)
 	{
-		if (num == 2)
-			putchar(num);
+		putchar((num % 10) + '0');
+		if (num == 3)
+			continue;
+
 		putchar(',');
-		putchar(' ');
+		putchar(' ');	
+	
 	}
+
+	putchar('\n');
 
 	return (0);
 }
