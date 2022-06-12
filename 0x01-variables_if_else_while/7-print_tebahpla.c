f19eee97954372206301698a29a5ec8a8745006b
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-11 22:01:20 -0700

    “7-print_tebahpla.c”

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; --alphabet)
+	{
+		putchar("%c", alphabet);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}			
