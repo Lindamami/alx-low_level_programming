21349cebf5f129c62067424e12aabf9fb29fcdbe
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-13 18:03:40 -0700

    divides string by 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string, followed by a new line.
+ * puts2 - prints half of a string, followed by a new line.
  * @str: input string
  * Return: no return
  */
-void puts_half(char *str)
+void puts2(char *str)
 {
 	int count = 0;
 
