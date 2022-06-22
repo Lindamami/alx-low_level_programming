04c67f1830da4dbe99f37d29454fdbd2edc487e6
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-22 11:55:28 -0700

    prints string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s);
+{
+	_putchar("%c\n", s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,8 @@
+Recursion
+What is recursion
+
+How to implement recursion
+
+In what situations you should implement recursion
+
+In what situations you shouldn’t implement recursion
