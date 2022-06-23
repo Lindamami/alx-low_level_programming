576b8e3435532546fadbba9818816eb076fd0f89
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-22 20:09:16 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,13 +11,10 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else
-	{
-		if (n == 0)
+	else if (n == 0)
 		{
 			return (1);
 		}
 	}
-	if (n > 0)
-		return (n * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
