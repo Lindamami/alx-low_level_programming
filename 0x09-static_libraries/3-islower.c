ceb892d0c14c7454f0be7db92cd173d132881fd8
Lindamami <goodnessnwaneri3@gmail.com>
2022-06-24 07:58:05 -0700

    static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+
+{
+
+if (c > 97 && c < 122)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
